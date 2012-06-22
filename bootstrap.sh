9f68e19e41c9db08ff0212f0b30cc4ea3eb8d15f
Gabriel Garcia Fdez <info@gabrielgciafdez.com>
2012-06-22 21:46:23 +0200

    Added some fixes to the bootstrap script and changed the README accordingly

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,7 +11,7 @@ die() {
     exit 1
 }
 
-echo "thanks for installing spf13-vim\n"
+echo "thanks for installing spf13-vim modified by gabrielgciafdez\n"
 
 # Backup existing .vim stuff
 echo "backing up current vim config\n"
@@ -20,7 +20,7 @@ for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc; do [ -e $i ] && mv $i $i.$today;
 
 
 echo "cloning spf13-vim\n"
-git clone --recursive -b 3.0 http://github.com/spf13/spf13-vim.git $endpath
+git clone --recursive -b 3.0 http://github.com/gabrielgciafdez/spf13-vim.git $endpath
 mkdir -p $endpath/.vim/bundle
 ln -s $endpath/.vimrc $HOME/.vimrc
 ln -s $endpath/.vim $HOME/.vim
